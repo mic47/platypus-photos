ecf6eadf8c560c2a4dddccac22fe785909f023c6
Michal Nanasi <michal@merlon.ai>
2024-05-09 22:55:22 +0200

    Add md5

--- annotate.py
+++ annotate.py
@@ -7,14 +7,15 @@ from dataclasses import dataclass
 
 from image_to_text import Models, ImageClassification
 from image_exif import Exif, ImageExif
+from md5_annot import MD5er, MD5Annot
 from geolocation import Geolocator, GeoAddress
-from cache import JsonlCache
+from cache import JsonlCache, HasImage
 
 
-@dataclass_json
 @dataclass
-class ImageAnnotations:
+class ImageAnnotations(HasImage):
     image: str
+    md5: str
     exif: ImageExif
     address: t.Optional[GeoAddress]
     text_classification: t.Optional[ImageClassification]
@@ -24,10 +25,12 @@ if __name__ == "__main__":
     models = Models()
     exif = Exif()
     geolocator = Geolocator()
+    md5 = MD5er()
     paths = sys.argv[1:]
     itt_cache = JsonlCache("output-image-to-text.jsonl", ImageClassification)
     exif_cache = JsonlCache("output-exif.jsonl", ImageExif)
     geo_cache = JsonlCache("output-geo.jsonl", GeoAddress)
+    md5_cache = JsonlCache("output-md5.jsonl", MD5Annot)
 
     try:
         for path in tqdm(paths, total=len(paths), desc="Image batches"):
@@ -38,16 +41,21 @@ if __name__ == "__main__":
             else:
                 # Because typecheck
                 exif_item = exif_item_m
-            if exif_item.gps is not None:
-                geo = geo_cache.get(path)
-                if geo is None:
-                    geo = geolocator.address(path, exif_item.gps.latitude, exif_item.gps.longitude)
-                    geo_cache.add(geo)
+            geo = geo_cache.get(path)
+            if geo is None and exif_item.gps is not None:
+                geo = geolocator.address(path, exif_item.gps.latitude, exif_item.gps.longitude)
+                geo_cache.add(geo)
             itt = itt_cache.get(path)
             if itt is None:
                 itt = list(models.process_image_batch([path]))[0]
                 itt_cache.add(itt)
-        img = ImageAnnotations(path, exif_item, geo, itt)
+            md5hsh_ret = md5_cache.get(path)
+            if md5hsh_ret is None:
+                md5hsh = md5.process(path)
+                md5_cache.add(md5hsh)
+            else:
+                md5hsh = md5hsh_ret
+            img = ImageAnnotations(path, md5hsh.md5, exif_item, geo, itt)
         print(img)
     finally:
         del itt_cache
---
+++ md5_annot.py
@@ -0,0 +1,18 @@
+from dataclasses_json import DataClassJsonMixin
+from cache import HasImage
+import hashlib
+from dataclasses import dataclass
+
+
+@dataclass
+class MD5Annot(HasImage):
+    image: str
+    md5: str
+
+
+class MD5er:
+    def __init__(self):
+        pass
+
+    def process(self, image: str) -> MD5Annot:
+        return MD5Annot(image, hashlib.md5(open("output-exif.jsonl", "rb").read()).hexdigest())
