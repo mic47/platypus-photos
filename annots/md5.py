26dc7c329db26ccaed18fcc9a173bc57ede153b6
Michal Nanasi <michal@merlon.ai>
2024-05-31 09:00:45 +0200

    Fix memory issue with big files

--- annots/md5.py
+++ annots/md5.py
@@ -4,4 +4,8 @@ from data_model.features import PathWithMd5
 
 
 def compute_md5(path: str) -> PathWithMd5:
-    return PathWithMd5(path, hashlib.md5(open(path, "rb").read()).hexdigest())
+    with open(path, "rb") as f:
+        md5 = hashlib.md5()
+        for x in f:
+            md5.update(x)
+        return PathWithMd5(path, md5.hexdigest())
