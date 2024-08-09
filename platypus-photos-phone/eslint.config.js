b42042901a0d18cbd46ff8b6c2b13d3bbf1b3bc5
Michal Nanasi <michal.nanasi@gmail.com>
2024-08-09 14:10:10 +0200

    Remove this config

---
+++ platypus-photos-phone/eslint.config.js
@@ -0,0 +1,8 @@
+// https://docs.expo.dev/guides/using-eslint/
+const expo = require('eslint-config-expo')
+
+module.exports = [expo, {
+        ignores: [
+                "scripts/reset-project.js"
+        ]
+}];
