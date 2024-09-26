ba9d3ca6d27d0a2f113241bc16306e6931a1aeb6
Michal Nanasi <michal.nanasi@gmail.com>
2024-09-26 18:42:19 +0200

    black

--- web/download_models.py
+++ web/download_models.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
 
     pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
     import ultralytics
+
     for model in ["yolov8x.pt", "yolov8x-cls.pt"]:
         ultralytics.YOLO(model)
